46127fdec86cf3fffeccbb18c75c99aa7f889a5c
Travis <travisfinch01@gmail.com>
2022-01-14 11:56:44 -0500

    no new lines

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -477,11 +477,10 @@ public:
                     if (genesis.nNonce == 0) {
                         ++genesis.nTime;
                     }
-                    printf("Mainnet:\n");
-                    printf("\rnonce %08x\n", genesis.nNonce);
-                    printf("\rtime: %u\n", genesis.nTime);
-                    printf("\rhash: 0x%s\n", genesis.GetHash().ToString().c_str());
-                    printf("\rmerklehash: 0x%s\n", genesis.hashMerkleRoot.ToString().c_str());
+                    printf("\rnonce %08x", genesis.nNonce);
+                    printf("\rtime: %u", genesis.nTime);
+                    printf("\rhash: 0x%s", genesis.GetHash().ToString().c_str());
+                    printf("\rmerklehash: 0x%s", genesis.hashMerkleRoot.ToString().c_str());
                 }
                 LogPrintf("Mainnet:\n");
                 LogPrintf("-nonce: %u\n", genesis.nNonce);
