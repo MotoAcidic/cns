f0cb3ab44d690acc48be4c411c30bc0230d9d2e7
TFinch <nexusnow@protonmail.com>
2022-01-13 20:43:52 -0500

    next one

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -466,11 +466,36 @@ public:
         nDefaultPort = 10226;
         nPruneAfterHeight = 100000;
 
+        static bool regenerate = true;
+
+
+        if (regenerate) {
+            consensus.hashGenesisBlock = uint256S("");
+            genesis.nNonce = 0;
+            arith_uint256 hashTarget = arith_uint256().SetCompact(genesis.nBits);
+            if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
+                while (UintToArith256(genesis.GetHash()) > hashTarget) {
+                    ++genesis.nNonce;
+                    if (genesis.nNonce == 0) {
+                        ++genesis.nTime;
+                    }
+                }
+                LogPrintf("Mainnet:\n");
+                LogPrintf("-nonce: %u\n", genesis.nNonce);
+                LogPrintf("-time: %u\n", genesis.nTime);
+                LogPrintf("-hash: 0x%s\n", genesis.GetHash().ToString().c_str());
+                LogPrintf("-merklehash: 0x%s\n", genesis.hashMerkleRoot.ToString().c_str());
+            }
+        } else {
+            assert(consensus.hashGenesisBlock == uint256S("0x00153528fa2c14fae39379d9d522726b29cb5c31608c170d13344a9986b9d51f"));
+            assert(genesis.hashMerkleRoot == uint256S("0x07887fde97c010c16ca9947d9d7c7cf291fc88e33b62b0347da50128e642930d5"));
+        }
+
         //FindMainNetGenesisBlock(1640529614, 0x20001fff, "main");
-        genesis = CreateGenesisBlock(1640529614, 3905, 0x20001fff, 1, 50 * COIN);
-        consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00153528fa2c14fae39379d9d522726b29cb5c31608c170d13344a9986b9d51f"));
-        assert(genesis.hashMerkleRoot == uint256S("07887fde97c010c16ca9947d9d7c7cf291fc88e33b62b0347da50128e642930d5"));
+        //genesis = CreateGenesisBlock(1640529614, 3905, 0x20001fff, 1, 50 * COIN);
+        //consensus.hashGenesisBlock = genesis.GetHash();
+        //assert(consensus.hashGenesisBlock == uint256S("0x00153528fa2c14fae39379d9d522726b29cb5c31608c170d13344a9986b9d51f"));
+        //assert(genesis.hashMerkleRoot == uint256S("07887fde97c010c16ca9947d9d7c7cf291fc88e33b62b0347da50128e642930d5"));
 
         vSeeds.emplace_back("seed00.cns.com", true);
         vSeeds.emplace_back("seed01.cns.com", true);
